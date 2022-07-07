f93885468a62287af6620180831fac1ba838c3d6
Hamid Hassani <hamidhassani119@gmail.com>
2022-07-07 20:37:46 +0430

    Navbar added to the template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.html"],
+  darkMode: "class",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
